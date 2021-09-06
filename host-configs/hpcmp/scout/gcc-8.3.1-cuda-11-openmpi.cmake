089460786a4fd304832555dc2d9884d2c45a1801
George Zagaris <george.zagaris@us.af.mil>
2021-09-06 11:37:18 -0400

    ENH: use OpenMPI from CREATE-AV/Helios modules

--- host-configs/hpcmp/scout/gcc-8.3.1-cuda-11-openmpi.cmake
+++ host-configs/hpcmp/scout/gcc-8.3.1-cuda-11-openmpi.cmake
@@ -14,7 +14,7 @@ set(CMAKE_CUDA_ARCHITECTURES "70" CACHE STRING "")
 #------------------------------------------------------------------------------
 # MPI
 #------------------------------------------------------------------------------
-set(MPI_BIN_ROOT /p/app/compiler/ppc64le/el8/nvidia/hpc_sdk/Linux_ppc64le/20.11/comm_libs/mpi/bin CACHE PATH "MPI bin PATH")
+set(MPI_BIN_ROOT /p/app/restricted/create/av/helios/externals/scout_tools/openmpi-4.0.5/bin CACHE PATH "MPI bin PATH")
 set(MPI_C_COMPILER ${MPI_BIN_ROOT}/mpicc CACHE PATH "MPI C compiler" )
 set(MPIC_CXX_COMPILER ${MPI_BIN_ROOT}/mpicxx CACHE PATH "MPI CXX compiler")
 set(MPI_Fortran_COMPILER ${MPI_BIN_ROOT}/mpif90 CACHE PATH "MPI Fortran compiler")
